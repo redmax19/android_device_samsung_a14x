3db0576cf8bc006d42df780b507d743a3654a0e2
ALESSANDRO |   R8S <alessandroaraujo0098@gmail.com>
2026-09-11 19:27:54 -0300

    Replace omni lunch combos with twrp variants

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,6 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-add_lunch_combo omni_a14x-user
-add_lunch_combo omni_a14x-userdebug
-add_lunch_combo omni_a14x-eng
+add_lunch_combo twrp_a14x-user
+add_lunch_combo twrp_a14x-userdebug
+add_lunch_combo twrp_a14x-eng
